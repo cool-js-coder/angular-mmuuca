4f7cc9927d188ebbf82c0f213510c528fb298a5d
cool-js-coder <mailtodeepak.code@gmail.com>
2020-10-26 22:04:16 +0530

    2 files modified

--- app/table-basic-example.ts
+++ app/table-basic-example.ts
@@ -18,6 +18,9 @@ import {
 export class TableBasicExample implements OnInit, AfterViewInit {
   @ViewChildren("tableCell") table: QueryList<any>;
   @ViewChild("tableWraper") tableWraper;
+  viewChange: boolean = false;
+  elmState = [];
+  selectedCell = null;
   displayedColumns = [
     "position",
     "name",
@@ -31,22 +34,63 @@ export class TableBasicExample implements OnInit, AfterViewInit {
   dataSource = ELEMENT_DATA;
   ngOnInit() {}
   ngAfterViewInit() {
-    this.getTableCellPosition();
+    setTimeout(() => {
+      this.viewChange = true;
+      this.getTableCellPosition();
+    });
   }
   getTableCellPosition() {
-    this.table.map(res => {
-      console.log(this.isInViewport(res.nativeElement));
+    console.log(this.table);
+    this.table.map((res, index) => {
+      console.log(res);
+      this.isInViewport(res.nativeElement, index);
     });
-    console.log(this.tableWraper.nativeElement.offsetWidth);
+    console.log(this.selectedCell);
   }
-  isInViewport(element) {
+
+  getAvailableColumn() {
+    for (let index in this.elmState) {
+      if (
+        !this.elmState[index].compliteListElm &&
+        this.elmState[index].partialListElm
+      ) {
+        this.elmState[index].cellIndex = this.displayedColumns[index];
+        return this.elmState[index];
+      } else if (
+        !this.elmState[index].compliteListElm &&
+        !this.elmState[index].partialListElm
+      ) {
+        const ind = parseInt(index, 10) - 1;
+        this.elmState[ind].cellIndex = this.displayedColumns[ind];
+        return this.elmState[ind];
+      }
+    }
+  }
+  isInViewport(element, index) {
     const rect = element.getBoundingClientRect();
-    console.log(rect);
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.right <= this.tableWraper.nativeElement.offsetWidth
-    );
+    const elm = {
+      compliteListElm:
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.right <= this.tableWraper.nativeElement.offsetWidth,
+      partialListElm:
+        rect.right - element.offsetWidth <=
+        this.tableWraper.nativeElement.offsetWidth,
+      elmWidthBetween:
+        this.tableWraper.nativeElement.offsetWidth -
+        (rect.right - element.offsetWidth),
+      cellIndex: null
+    };
+    if (this.selectedCell === null) {
+      if (!elm.compliteListElm && elm.partialListElm) {
+        elm.cellIndex = this.displayedColumns[index];
+        this.selectedCell = elm;
+      } else if (!elm.compliteListElm && !elm.partialListElm) {
+        const ind = parseInt(index, 10) - 1;
+        elm.cellIndex = this.displayedColumns[ind];
+        this.selectedCell = elm;
+      }
+    }
   }
 }
 
